ae7ee0f315988df7e5e880406c284cfbdd7269c0
Heath Westbrook <hwestbrook@gmail.com>
2012-07-28 10:11:26 -1000

    fixed the PHP proxies to use cURL instead of fopen
    changed:
    
    - PHP proxies now use cURL instead of fopen, as the web server did not
    like fopen.
    - add some tweaks to the fonts used so they are more readable.

--- proxy_train-secondbus.php
+++ proxy_train-secondbus.php
@@ -1,6 +1,19 @@
 <?php
+
+// this disallows cross site attacks
 header('Access-Control-Allow-Origin: *');
+
+// this specifies the API URL call
 $url = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=J&s=4006&useShortTitles=true";
-$str = file_get_contents($url);
+
+// this sets up cURL and gets the data
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$str = curl_exec($ch);
+curl_close($ch);
+
+// this returns the data
 echo $str;
+
 ?>
--- proxy_weather.php
+++ proxy_weather.php
@@ -1,6 +1,17 @@
 <?php
+// this disallows cross site attacks
 header('Access-Control-Allow-Origin: *');
+
+// this specifies the API URL call
 $url = "http://api.wunderground.com/api/406f56b313b47308/conditions/forecast/q/pws:MSFOC1.json";
-$str = file_get_contents($url);
+
+// this sets up cURL and gets the data
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$str = curl_exec($ch);
+curl_close($ch);
+
+// this returns the data
 echo $str;
 ?>
